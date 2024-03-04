46d532ac43feb927129d27a766a1f9f1595e14c3
Sup682 <supriya88swami@gmail.com>
2024-03-04 12:01:18 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GLFirstWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
